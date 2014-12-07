7b7b21313e3a3bfd61952c47c89dd32036af61bb
rajins <sasi.rajinkumar@gmail.com>
2014-12-07 20:52:11 +0530

    Rajin Sasi | Project Euler | Program1

--- project-euler/Program1.java
+++ project-euler/Program1.java
@@ -3,3 +3,26 @@
   Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+import java.lang.System;
+import java.util.Scanner;
+
+public class Program1 {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a maximum number... ");
+
+    long n = scanner.nextLong();
+    long result = 0;
+    while (n > 0) {
+      for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+          result = result + i;
+        }
+      }
+      System.out.println("Result is: "+result);
+    }
+  }
+}
+
